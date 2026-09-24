339889fd7a29e8a88bf0bffd55f592a5b7b77d6a
pupsikozz03-dot <pupsikozz03@gmail.com>
2026-09-24 21:03:42 +0300

    Update ModMixins.java

--- src/main/java/com/visuals/mixin/ModMixins.java
+++ src/main/java/com/visuals/mixin/ModMixins.java
@@ -5,30 +5,12 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.render.RenderTickCounter;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 public class ModMixins {
-
-    @Mixin(net.minecraft.client.render.GameRenderer.class)
-    public static class MixinGameRenderer {
-        // В 1.21.x аргумент FOV имеет тип double!
-        @Inject(method = "getBasicProjectionMatrix", at = @At("RETURN"), cancellable = true)
-        private void onGetBasicProjectionMatrix(double fov, CallbackInfoReturnable<Matrix4f> cir) {
-            if (VisualModClient.INSTANCE == null) return;
-            VisualModClient.AspectRatioModule mod = (VisualModClient.AspectRatioModule) VisualModClient.INSTANCE.getModuleManager().getModule(VisualModClient.AspectRatioModule.class);
-            if (mod != null && mod.isEnabled()) {
-                float customAspect = mod.getRatio();
-                Matrix4f matrix = new Matrix4f();
-                matrix.perspective((float) Math.toRadians(fov), customAspect, 0.05f, 1000.0f);
-                cir.setReturnValue(matrix);
-            }
-        }
-    }
 
     @Mixin(net.minecraft.client.gui.hud.InGameHud.class)
     public static class MixinInGameHud {
